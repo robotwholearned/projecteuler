98630cc078d73aaa604979411ae158a93c895ecf
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-08-12 01:25:41 -0600

    Problem1 - Multiples of 3 and 5

--- ProjectEuler/main.m
+++ ProjectEuler/main.m
@@ -8,10 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+int sumMultiplesOf3or5below1000();
+
+int main(int argc, const char* argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"%d", sumMultiplesOf3or5below1000());
+    }
     return 0;
+}
+
+int sumMultiplesOf3or5below1000()
+{
+    /* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        Find the sum of all the multiples of 3 or 5 below 1000.
+    */
+    int sum = 0;
+
+    for (int i = 0; i < 1000; i++) {
+        NSLog(@"%d", i);
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+            NSLog(@"Sum: %d", sum);
+        }
+    }
+
+    return sum;
 }
