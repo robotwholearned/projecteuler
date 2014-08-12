ddfa1dc916d3789d695c91432f33e792208d8519
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-08-12 01:14:40 -0600

    Initial Commit

---
+++ ProjectEuler/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProjectEuler
+//
+//  Created by Sandquist, Cassandra G on 8/12/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
